636a037073a04d335c0f3f0e54aafe4679334526
Waldomiro <38801006+Waldomiro1998@users.noreply.github.com>
2020-03-10 12:28:08 -0400

    novo split

--- analisadorLexico.py
+++ analisadorLexico.py
@@ -1,37 +1,49 @@
-def dicAnalise(token):
-    global linha
-    try:
-        return dic[token]
-    except KeyError:
-        if (token.isalnum() and token[0].isalpha()):
-            tokensId.append(token)
-            return 'TOKEN_ID'
-        else:
-            erros.append((linha, token))
+import re 
+def tokenTranslate(token):
+	global linha
+	if(token=='var'):
+		return 'TOKEN.VAR'
+	elif(token=='integer'):
+		return 'TOKEN.INTEGER'	
+	elif(token=='real'):
+		return 'TOKEN.REAL'
+	elif(token=='if'):
+		return 'TOKEN.IF'
+	elif(token=='then'):
+		return 'TOKEN.THEN'
+	elif(token==':'):
+		return 'TOKEN.DOT2'						
+	elif(token==','):
+		return 'TOKEN.COMMA'		
+	elif(token==';'):
+		return 'TOKEN.SEMICOLON'
+	elif(token==':='):
+		return 'TOKEN.ASSIGNMENT'
+	elif(token=='+'):
+		return 'TOKEN.OPSUM'
+	else:							
+		if (token.isalnum() and token[0].isalpha()):
+			tokensId.append(token)
+			return 'TOKEN.ID'
+		else:
+			erros.append((linha, token))
 def tokenizar(arquivo):
 	global linha
-	arquivo = arquivo.replace(':', ' : ')
-	arquivo = arquivo.replace(": =", ' := ')
-	arquivo = arquivo.replace('+', ' + ')
-	arquivo = arquivo.replace(',', ' , ')
-	arquivo = arquivo.replace(';', ' ; ')
-	arquivo = arquivo.replace('\n', ' \n ')
-	arquivo = arquivo.split(' ')
+	arquivo =re.split(r'(;|,|:=|:|\n|[^a-z0-9])',arquivo)
+	while("" in arquivo): 
+		arquivo.remove("")
+	while(" " in arquivo): 
+		arquivo.remove(" ")  
 	for palavra in arquivo:
 		if palavra == '\n':
 			linha += 1
 		elif palavra != "":
-			tokens.append(dicAnalise(palavra))
+			tokens.append(tokenTranslate(palavra))
 			linhas.append(linha)
 	if len(erros) > 0:
 		erro = ''
 		for i in erros:
 			erro += "Erro Léxico, Linha {}: Não é valído o identificador {} \n".format(i[0], i[1])
 		print(erro)
-
 tokens,erros, tokensId,linhas = [], [], [], []
 linha=1
-dic = {'var': 'TOKEN_VAR', 'integer': 'TOKEN_INTEGER', 'real': 'TOKEN_REAL',
-            'if': 'TOKEN_IF', 'then': 'TOKEN_THEN', ':': 'TOKEN_DOT2',
-            ',': 'TOKEN_COMMA', ';': 'TOKEN_SEMICOLON', ':=': 'TOKEN_ASSIGNMENT',
-            '+': 'TOKEN_OPSUM'}
